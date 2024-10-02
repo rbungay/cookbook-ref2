f3be8e9da2c162ed3e66b0d453a13cd0c1239ebc
Rani Bungay <ranibungay@gmail.com>
2024-10-02 11:29:30 -0400

    add new recipe

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -5,9 +5,15 @@ const router = express.Router();
 
 const User = require("../models/user.js");
 const Recipe = require("../models/recipe.js");
+const Ingredient = require("../models/ingredient.js");
 
 router.get("/", async (req, res) => {
   res.render("recipes/index.ejs");
+});
+
+router.get("/new", async (req, res) => {
+  const ingredients = await Ingredient.find({});
+  res.render("recipes/new.ejs", { ingredients });
 });
 
 module.exports = router;
